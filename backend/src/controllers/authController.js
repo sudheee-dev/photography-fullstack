33fe14a245553a31c106ea8ff77c42080f341725
sudheer Sub <sudheerbhagavatula@gmail.com>
2026-07-19 04:43:21 +0530

    Add Cloudinary image upload and fix Angular environment configuration

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,6 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const registerUser = async (req, res) => {
+  const normalizedGender = gender?.toLowerCase();
   const {
     email,
     password,
